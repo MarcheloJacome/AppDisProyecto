e535cd4968738b8f897274cd20f8dd9baae32cbf
MarcheloJacome <marcelo.jacome@hotmail.com>
2021-07-11 23:26:37 -0500

    Update ShoppingServiceApplication.java

--- shopping-service/src/main/java/academy/digitallab/store/shopping/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/ShoppingServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShoppingServiceApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingServiceApplication.class, args);
 	}
